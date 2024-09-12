55e43caad36c2f2bdb309dcd1ac6359c902f58f7
gusty <gustavodiorlando@gmail.com>
2024-09-12 16:10:33 -0300

    Add images, basic display and figure out coordinate logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 
 function App() {
+
   return (
     <>
-      j
+    <div className="topBar">
+      <h1>Where&apos;s Waldo</h1>
+    </div>
+    <div className='mapContainer'>
+      <img src="/images/waldo-map.jpeg" alt="waldo map" className='map' onClick={(e) => console.log(
+        e.nativeEvent.offsetX, e.nativeEvent.offsetY)} />
+    </div>
     </>
+    
   )
 }
 
