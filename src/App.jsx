a2bdde0c618aeae76db53c25a842f0aea67dd79b
gusty <gustavodiorlando@gmail.com>
2024-09-13 14:16:59 -0300

    Create dropdown menu on clicking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [menuActive, setMenuActive] = useState(false);
+  const [coords, setCoords] = useState({x: 0, y: 0});
 
   return (
     <>
@@ -8,11 +12,14 @@ function App() {
       <h1>Where&apos;s Waldo</h1>
     </div>
     <div className='mapContainer'>
-      <img src="/images/waldo-map.jpeg" alt="waldo map" className='map' onClick={(e) => console.log(
-        e.nativeEvent.offsetX, e.nativeEvent.offsetY)} />
+      <img src="/images/waldo-map.jpeg" alt="waldo map" className='map' onClick={(e) => {
+      setCoords({x: e.clientX, y: e.clientY})
+      setMenuActive(!menuActive) }
+      }/>
+      <Dropdown active={menuActive} coords={coords}></Dropdown>
     </div>
-    </>
     
+    </>
   )
 }
 
